a234f065d113d2334282295a42ca25d5455a48ae
Mohey Eldeen Maher <54249650+mohey-e-maher@users.noreply.github.com>
2024-05-01 00:10:41 +0300

    setup

--- (2) Final Project/raspberry-pi_code.py
+++ (2) Final Project/raspberry-pi_code.py
@@ -45,3 +45,35 @@ secretkey = "9922"
 delay = 1
 
 
+#define the write & earse to the screen
+GPIO.setup(LCD_E, GPIO.OUT)  # E (Earse the screen)
+GPIO.setup(LCD_RS, GPIO.OUT) # RS (write to the screen)
+# the first 4 binary input
+GPIO.setup(LCD_D4, GPIO.OUT) # DB4
+GPIO.setup(LCD_D5, GPIO.OUT) # DB5
+GPIO.setup(LCD_D6, GPIO.OUT) # DB6
+GPIO.setup(LCD_D7, GPIO.OUT) # DB7
+
+#define the keypad buttons
+GPIO.setup(R1, GPIO.OUT) # DB7
+GPIO.setup(R2, GPIO.OUT) # DB7
+GPIO.setup(R3, GPIO.OUT) # DB7
+GPIO.setup(R4, GPIO.OUT) # DB7
+GPIO.setup(C1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(C2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(C3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(C4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+#define sensors pins
+GPIO.setup(Rain_sensor, GPIO.IN)
+GPIO.setup(flame_Sensor, GPIO.IN)
+GPIO.setup(temp_level, GPIO.IN)
+
+# Define device screen constants
+LCD_WIDTH = 16    # Maximum characters per line
+LCD_CHR = True
+LCD_CMD = False
+LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
+LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
+
+
